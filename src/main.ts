d4603478a266fda93d9a1a4c7f3ed0c522ce1790
Fernando Sotelo <al169888@alumnos.uacj.mx>
2024-08-31 12:53:18 -0600

    wip

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
     }),
   );
   app.setGlobalPrefix('api');
-  await app.listen(3000, '0.0.0.0');
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
