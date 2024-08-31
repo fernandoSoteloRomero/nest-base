bafafbb7c7eedadceafba836d2d2a69845199f7e
Fernando Sotelo <al169888@alumnos.uacj.mx>
2024-08-31 12:35:18 -0600

    asd

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,6 @@ import { ClientsModule } from './clients/clients.module';
         ssl:
           process.env.STAGE === 'prod' ? { rejectUnauthorized: false } : null,
       },
-
       type: 'postgres',
       host: process.env.DB_HOST, //localhost
       port: +process.env.DB_PORT,
